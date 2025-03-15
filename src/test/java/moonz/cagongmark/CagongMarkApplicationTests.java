cd451718563839c9daf32e87c751c723125066d5
BananMoon <younji1115@gmail.com>
2025-03-15 17:19:30 +0900

    feat: project 초기화한다.

---
+++ src/test/java/moonz/cagongmark/CagongMarkApplicationTests.java
@@ -0,0 +1,13 @@
+package moonz.cagongmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CagongMarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
